1aeeabc87904494dae287594fa42d6005c9f8513
Encho Donchev <esdonchev@gmail.com>
2022-11-23 11:19:15 +0200

    First commit

---
+++ Data/MoiteRecepti.Data.Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿namespace MoiteRecepti.Data.Models
+{
+    using System.Collections.Generic;
+
+    using MoiteRecepti.Data.Common.Models;
+
+    public class Ingredient : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public virtual ICollection<RecipeIngredient> Recipes =>
+            new HashSet<RecipeIngredient>();
+    }
+}
---
+++ Data/MoiteRecepti.Data.Models/Recipe.cs
@@ -0,0 +1,27 @@
+﻿namespace MoiteRecepti.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    using MoiteRecepti.Data.Common.Models;
+
+    public class Recipe : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public TimeSpan PreparationTime { get; set; }
+
+        public TimeSpan CookingTime { get; set; }
+
+        public int PortionsCount { get; set; }
+
+        public string AddedByUserId { get; set; }
+
+        public virtual ApplicationUser AddedByUser { get; set; }
+
+        public virtual ICollection<RecipeIngredient> Ingredients =>
+            new HashSet<RecipeIngredient>();
+    }
+}
