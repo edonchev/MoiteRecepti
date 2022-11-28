06d3f7970354783aff2798ecf3f7da64653e1236
edonchev <esdonchev@gmail.com>
2022-11-28 11:48:02 +0200

    Image model added

---
+++ Data/MoiteRecepti.Data.Models/Image.cs
@@ -0,0 +1,23 @@
+﻿namespace MoiteRecepti.Data.Models
+{
+    using System;
+    using MoiteRecepti.Data.Common.Models;
+
+    public class Image : BaseModel<string>
+    {
+        public Image()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string AddedByUserId { get; set; }
+
+        public ApplicationUser AddedByUser { get; set; }
+
+        public string Extension { get; set; }
+
+        public int RecipeId { get; set; }
+
+        public virtual Recipe Recipe { get; set; }
+    }
+}
